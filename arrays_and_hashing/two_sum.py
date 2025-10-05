258ed7d5187b2f9c63a1f69715edb2f93d6c4246
TheRonnieSouza <ronniesouza.contato@gmail.com>
2025-10-04 23:56:53 -0300

    fix

--- arrays_and_hashing/two_sum.py
+++ arrays_and_hashing/two_sum.py
@@ -4,13 +4,10 @@ from typing import List
 class SolutionTwoSum:
     
     def two_sum(self, nums: List[int], target: int) -> List[int]:
-        result= []
-        for i, num in enumerate(nums):            
-            size = len(nums)
-            print(f"índice: {i}, valor: {num}, size: {size}")
-            if (size - 1) > i:
-                print(nums[i])
-                if (nums[i] + nums[i + 1]) == target:
-                    result.append(i)
-                    result.append(i+1)
-        return result
+        mapa = {}
+        
+        for i, num in enumerate(nums):
+            complemento = target - num 
+            if complemento in mapa:
+                return [mapa[complemento], i]
+            mapa[num] = i
