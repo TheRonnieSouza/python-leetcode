0356323248fedae142ad5b7955805371d79ab9a7
TheRonnieSouza <ronniesouza.contato@gmail.com>
2025-10-06 23:35:56 -0300

    feat: is palindrome logic

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from arrays_and_hashing.contains_duplicate import Solution
 from arrays_and_hashing.valid_anagram import SolutionValidAnagram
 from arrays_and_hashing.group_anagrams import SolutionGroupAnagrams
+from two_pointers.valid_palindrome import SolutionValidPalindrome
+
+## -------------------
+
+palindrome = SolutionValidPalindrome()
+
+#string = "A man, a plan, a canal: Panama"
+#string = "race a car"
+string = "    "
+
+if palindrome.isPalindrome(s= string):
+    print(f"o texto -> {string} <-- is a palindrome")
+else:
+    print(f"o texto -> {string} <-- is not a palindrome")
+
+## ------------------   groupAnagrams  ---------------
 
 
-group_anagrams = SolutionGroupAnagrams()
+#group_anagrams = SolutionGroupAnagrams()
 
-strs = ["eat","tea","tan","ate","nat","bat"]
-result = group_anagrams.groupAnagrams(strs)
-print(f" the result is : {result}")
+#strs = ["eat","tea","tan","ate","nat","bat"]
+#result = group_anagrams.groupAnagrams(strs)
+#print(f" the result is : {result}")
+
+
+## ------------------   test_is_anagram  ---------------
 
 #Solution.TestContainsDuplicate(1,2,3,1)
 
